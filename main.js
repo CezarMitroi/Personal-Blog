2999c6244df077491acfd3d35c2088457fe620e1
Cezar Mitroi <43640399+CezarMitroi@users.noreply.github.com>
2019-01-31 14:11:47 +0200

    update jquery

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
 $(document).ready(()=>{
+  
   $('.menu').on('click',(event)=>{
-    $(event.currentTarget).siblings().toggle();
+    $(event.currentTarget).siblings().fadeToggle(750);
   })
+  $('.about-button').on('click',()=>{
+    $('.about').slideToggle();
+  })
+
+$('.menu').on('mouseenter',(event)=>{
+  $(event.currentTarget).animate({
+      fontSize:'22px'
+    },600)
+}).on('mouseleave',(event)=>{
+  $(event.currentTarget).animate({
+      fontSize:'18px'
+    }, 600)
+})
+$('.navbar-brand').on('mouseenter',(event)=>{
+ $(event.currentTarget).css({
+   transform:'scale(2)',
+    transition: 'transform 1s'
+ })
+}).on('mouseleave',(event)=>{
+  $(event.currentTarget).css({
+    transform:'scale(1)'
+  })
+})
+$('.desc').on('mouseenter',(event)=>{
+
+})
+  $('.place').on('mouseenter',(event)=>{
+    $(event.currentTarget).css({
+      transform: 'scale(1.2)',
+      transition: 'transform 1s'
+    })
+  }).on('mouseleave',(event)=>{
+    $(event.currentTarget).css({
+      transform: 'scale(1)',
+      transition: 'transform 1s'
+    })
+  })
+  
+  
 })
